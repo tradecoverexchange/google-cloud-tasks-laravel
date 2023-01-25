690f6e0a5b9ab1ccf07f8ae7d2c023e47df7ac08
Peter Fox <peter.fox@peterfox.me>
2023-01-25 19:50:15 +0000

    Fix command test

--- tests/MonitorCommandTest.php
+++ tests/MonitorCommandTest.php
@@ -5,6 +5,7 @@ namespace TradeCoverExchange\GoogleCloudTaskLaravel\Tests;
 use Google\Cloud\Tasks\V2beta3\CloudTasksClient;
 use Google\Cloud\Tasks\V2beta3\Queue;
 use Google\Cloud\Tasks\V2beta3\QueueStats;
+use Illuminate\Support\Facades\App;
 use Mockery\MockInterface;
 use Orchestra\Testbench\TestCase;
 use TradeCoverExchange\GoogleCloudTaskLaravel\CloudTaskServiceProvider;
@@ -34,19 +35,31 @@ class MonitorCommandTest extends TestCase
 
         $this->client
             ->shouldReceive('getQueue')
-            ->with('projects/test/locations/europe-west1/queues/default', \Mockery::on(fn ($value) => is_array($value)))
+            ->with(
+                'projects/test/locations/europe-west1/queues/default',
+                \Mockery::on(fn ($value) => is_array($value))
+            )
             ->andReturn($cloudQueue);
 
         $cloudQueue->setStats($stats);
         $stats->setTasksCount(10);
 
-        $this->artisan('queue:monitor', [
-            'queues' => 'http_cloud_tasks:default',
-        ])
-            ->expectsTable(['Connection', 'Queue', 'Size', 'Status'], [[
-                'http_cloud_tasks', 'default', '10', 'OK',
-            ]])
-            ->assertExitCode(0);
+
+        if (version_compare(App::version(), '9.21.0', '>=')) {
+            $this->artisan('queue:monitor', [
+                'queues' => 'http_cloud_tasks:default',
+            ])
+                ->expectsOutputToContain('default')
+                ->assertSuccessful();
+        } else {
+            $this->artisan('queue:monitor', [
+                'queues' => 'http_cloud_tasks:default',
+            ])
+                ->expectsTable(['Connection', 'Queue', 'Size', 'Status'], [[
+                    'http_cloud_tasks', 'default', '10', 'OK',
+                ]])
+                ->assertExitCode(0);
+        }
     }
 
     protected function getPackageProviders($app)
